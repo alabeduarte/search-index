b36e97835b5045a1254d8342628b3ee616bad86f
Alabe Duarte <alabeduarte@gmail.com>
2019-07-04 09:34:52 +1000

    Enable es6 syntax

--- src/hello-world.test.js
+++ src/hello-world.test.js
@@ -1,7 +1,7 @@
-const helloWorld = require('./hello-world');
+import helloWorld from './hello-world';
 
 describe('Hello World', () => {
-	it('works', () => {
-		expect(helloWorld()).toEqual('Hello World');
-	});
+  it('works', () => {
+    expect(helloWorld()).toEqual('Hello World');
+  });
 });
